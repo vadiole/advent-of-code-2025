3670f6b30ceefa64ec218ffb7f52852a07dd79ca
Dimitrii <48314952+vadiole@users.noreply.github.com>
2025-12-01 20:05:45 +0100

    day 1 part 1

---
+++ day1.kt
@@ -0,0 +1,22 @@
+// https://adventofcode.com/2025/day/1
+fun main() {
+    val input = File("input.txt").readText()
+    val maxDirections = 100
+    var position = 50
+    var zeroHits = 0
+    for (line in input.lines()) {
+        val direction = line.take(1)
+        val rotations = line.drop(1).toInt()
+        val sign = when (direction) {
+            "L" -> -1
+            "R" -> 1
+            else -> error("Unknown direction: $direction")
+        }
+        position += sign * rotations
+        position = position.rem(maxDirections)
+        if (position == 0) {
+            zeroHits++
+        }
+    }
+    println("output: $zeroHits")
+}
