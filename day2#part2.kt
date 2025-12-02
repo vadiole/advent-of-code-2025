ad7efd3f44dceb2960107d86394407f2e15f6b97
Dimitrii <48314952+vadiole@users.noreply.github.com>
2025-12-02 18:34:19 +0100

    day 2 part 2

---
+++ day2#part2.kt
@@ -0,0 +1,22 @@
+// https://adventofcode.com/2025/day/2#part2
+fun main() {
+    val input = File("input.txt").readText()
+    val productIdRanges = input.split(",").map { range ->
+        val (start, end) = range.split("-").map { it.toLong() }
+        start..end
+    }
+    var invalidProductIdsSum = 0L
+    for (range in productIdRanges) {
+        for (productId in range) {
+            val productIdString = productId.toString()
+            for (partLength in 1..productIdString.length / 2) {
+                val parts = productIdString.chunked(partLength)
+                if (parts.all { it == parts[0] }) {
+                    invalidProductIdsSum += productId
+                    break
+                }
+            }
+        }
+    }
+    println("output: $invalidProductIdsSum")
+}
