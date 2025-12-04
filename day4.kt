566236d9958db0043f3881d3fc2e9638abb44717
Dimitrii <48314952+vadiole@users.noreply.github.com>
2025-12-04 15:36:26 +0100

    day 4 part 1

---
+++ day4.kt
@@ -0,0 +1,40 @@
+import java.io.File
+
+// https://adventofcode.com/2025/day/4
+fun main() {
+    val input = File("input.txt").readText()
+    val diagram = input.lines()
+    val adjacentPositionOffsets = listOf(
+        -1 to -1, -1 to +0, -1 to +1,
+        +0 to -1,  /*   */  +0 to +1,
+        +1 to -1, +1 to +0, +1 to +1,
+    )
+    val maxAdjacentRolls = 3
+    var totalAccessibleRolls = 0
+    for (i in diagram.indices) {
+        for (j in diagram[i].indices) {
+            if (diagram[i][j] != '@') continue
+            var adjacentRolls = 0
+            for ((offsetI, offsetJ) in adjacentPositionOffsets) {
+                val testI = i + offsetI
+                val testJ = j + offsetJ
+                if (testI !in diagram.indices) {
+                    continue
+                }
+                if (testJ !in diagram[testI].indices) {
+                    continue
+                }
+                if (diagram[testI][testJ] == '@') {
+                    adjacentRolls++
+                }
+                if (adjacentRolls > maxAdjacentRolls) {
+                    break
+                }
+            }
+            if (adjacentRolls <= maxAdjacentRolls) {
+                totalAccessibleRolls++
+            }
+        }
+    }
+    println("output: $totalAccessibleRolls")
+}
