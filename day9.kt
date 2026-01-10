b9f0875f45f34445816752e009160c82a0564cd3
Dimitrii <48314952+vadiole@users.noreply.github.com>
2026-01-10 02:47:31 +0100

    day 9 part 1

--- day9.kt
+++ day9.kt
@@ -1,5 +1,4 @@
 import java.io.File
-import kotlin.math.absoluteValue
 
 //https://adventofcode.com/2025/day/9
 fun main() {
@@ -13,9 +12,9 @@ fun main() {
         val (x1, y1) = coords[i]
         for (j in i + 1..coords.lastIndex) {
             val (x2, y2) = coords[j]
-            val width = (x2 - x1).absoluteValue + 1L
-            val height = (y2 - y1).absoluteValue + 1L
-            val square = width * height
+            val (left, right) = minOf(x1, x2) to maxOf(x1, x2)
+            val (top, bottom) = minOf(y1, y2) to maxOf(y1, y2)
+            val square = (right - left + 1L) * (bottom - top + 1L)
             if (square > maxRectSquare) {
                 maxRectSquare = square
             }
