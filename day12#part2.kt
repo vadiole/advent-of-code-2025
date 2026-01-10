69f85a56f50c034939e7f3f41bc7e6bf2975d9ee
Dimitrii <48314952+vadiole@users.noreply.github.com>
2026-01-10 03:06:44 +0100

    day 12 part 2

---
+++ day12#part2.kt
@@ -0,0 +1,14 @@
+/*
+Heey! This one was solved by ✨magic✨
+
+--- Part Two ---
+
+The Elves thank you profusely for the help and start rearranging the oddly-shaped presents. As you look up, you notice that a lot more Elves have arrived here at the Christmas tree farm.
+
+In fact, many of these new arrivals look familiar: they're the Elves you helped while decorating the North Pole base. Right on schedule, each group seems to have brought a star to put atop one of the Christmas trees!
+
+Before any of them can find a ladder, a particularly large Christmas tree suddenly flashes brightly when a large star magically appears above it! As your eyes readjust, you think you notice a portly man with a white beard disappear into the crowd.
+
+You go look for a ladder; only 23 stars to go.
+
+*/
